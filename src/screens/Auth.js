a6cf6d93c3594524479b4ca2a8be09510568be19
Fernando Veronez <fernandoveronez_@hotmail.com>
2020-04-16 23:01:52 -0300

    linked front and backend app

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -7,28 +7,47 @@ import {
   TouchableOpacity,
   Alert
 } from 'react-native'
+import axios from 'axios'
 import backgoundImage from '../../assets/imgs/login.jpg'
 import CommonStyles from '../commonStyles'
 import AuthInput from '../components/AuthInput'
+import { server, showError, showSuccess} from '../common'
 
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  stageNew: false,
+}
 export default class Auth extends Component {
 
   state = {
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    stageNew: false,
+    ...initialState
   }
 
   signinOrSigup = () =>{
     if(this.state.stageNew){
-      Alert.alert('Sucesso', 'Criar Conta')
+      this.singup()
     }else{
       Alert.alert('Sucesso', 'Logar')
     }
   }
 
+  singup = async () =>{
+    try {
+      await axios.post(`${server}/signup`, {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        confirmPassword: this.state.confirmPassword
+      })
+      showSuccess('Usuário cadastrado!')
+      this.setState({...initialState})
+    } catch (err) {
+      showError(err)
+    }
+  }
   render() {
     return (
       <ImageBackground source={backgoundImage} style={styles.background}>
@@ -91,8 +110,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.8)',
     padding: 20,
     width: '90%',
-
-
   },
   input: {
     marginTop: 10,
