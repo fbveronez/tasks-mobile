48eb47763ad0b27026614b1cbeb6b1d50ad747e4
Fernando Veronez <fernandoveronez_@hotmail.com>
2020-04-17 23:41:56 -0300

    added style to the drawer

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,48 +1,66 @@
 import React from 'react'
-import {createAppContainer, createSwitchNavigator} from 'react-navigation'
-import {createDrawerNavigator} from 'react-navigation-drawer'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 import TaskList from './screens/TaskList'
 import Auth from './screens/Auth'
+import Menu from './screens/Menu'
+import CommonStyles from './commonStyles'
 
+
+const menuConfig = {
+  initialRouteName: 'Today',
+  contentComponent: Menu,
+    contentOptions: {
+      labelStyle: {
+        fontFamily: CommonStyles.fontFamily,
+        fontWeight: 'normal',
+        fontSize: 20,
+      },
+      activeLabelStyle: {
+        color: '#080',
+        fontWeight: 'bold',
+      }
+    }
+}
 const menuRoutes = {
-  Today:{
+  Today: {
     name: 'Today',
     screen: props => <TaskList title='Hoje' daysAhead={0} {...props} />,
-    navigationOptions:{
+    navigationOptions: {
       title: 'Hoje'
     }
   },
-  Tomorrow:{
+  Tomorrow: {
     name: 'Tomorrrow',
     screen: props => <TaskList title='Amanhã' daysAhead={1} {...props} />,
-    navigationOptions:{
+    navigationOptions: {
       title: 'Amanhã'
     }
   },
-  Week:{
+  Week: {
     name: 'Week',
     screen: props => <TaskList title='Semana' daysAhead={7} {...props} />,
-    navigationOptions:{
+    navigationOptions: {
       title: 'Semana'
     }
   },
-  Month:{
+  Month: {
     name: 'Month',
     screen: props => <TaskList title='Mês' daysAhead={30} {...props} />,
-    navigationOptions:{
+    navigationOptions: {
       title: 'Mês'
     }
   },
 }
 
-const menuNavigator = createDrawerNavigator(menuRoutes)
+const menuNavigator = createDrawerNavigator(menuRoutes, menuConfig)
 
-const mainRoutes ={
-  Auth:{
+const mainRoutes = {
+  Auth: {
     name: 'Auth',
     screen: Auth
   },
-  Home:{
+  Home: {
     name: 'Home',
     screen: menuNavigator
   }
